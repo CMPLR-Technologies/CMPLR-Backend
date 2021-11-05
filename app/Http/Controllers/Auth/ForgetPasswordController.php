cdc0b10abdb1be1903c701d49d4ed3cd22bb745b
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-11-05 18:13:22 +0200

    finishing auth controllers

--- app/Http/Controllers/Auth/ForgetPasswordController.php
+++ app/Http/Controllers/Auth/ForgetPasswordController.php
@@ -56,4 +56,31 @@ class ForgetPasswordController extends Controller
     {
 
     }
+    /**
+     * @OA\Post(
+     * path="user/resend_verification_email",
+     * summary="resend verification email for the user",
+     * description="User can get verification for existing email",
+     * operationId="resendVerificationEmail",
+     * tags={"Auth"},
+     * @OA\Response(
+     *    response=200,
+     *    description="Successfully sent email to reset password",
+     *      @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Email sent to yousef.elftah00@eng-st.cu.edu.eg!")
+     *        )
+     *     )
+     * ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Unauthenticated"
+     *   ),
+     * )
+     */
+
+
+    public function resendVerificationEmail()
+    {
+
+    }
 }
