2f16a534d2c740a63371ac2ff96ab816daebfcff
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-11-05 21:03:20 +0200

    API documentation for deleting account

--- app/Http/Controllers/UsersettingController.php
+++ app/Http/Controllers/UsersettingController.php
@@ -12,31 +12,31 @@ class UsersettingController extends Controller
      * 		path="/settings/account",
      * 		summary="User setting",
      * 		description="Retrieve Account Setting for User.",
-     * 		operationId="GEtAccountSetting",
-     * 		tags={"User"},
+     * 		operationId="accountSettings",
+     * 		tags={"Settings"},
      *
      * @OA\Response(
      *    response=200,
-     *    description="sucess",
+     *    description="Success",
      *    @OA\JsonContent(
      *       type="object",
-     *       @OA\Property(property="Meta", type="object",
-     *          @OA\Property(property="Status", type="integer", example=200),
+     *       @OA\Property(property="meta", type="object",
+     *          @OA\Property(property="status", type="integer", example=200),
      *           @OA\Property(property="msg", type="string", example="OK"),
      *        ),
      *       @OA\Property(property="response", type="object",          
      *                      @OA\Property(
-     *                         property="Email",
+     *                         property="email",
      *                         type="string",
      *                         example="example@gmail.com"
      *                      ),
      *                      @OA\Property(
-     *                         property="Password",
+     *                         property="password",
      *                         type="string",
      *                         example="password123"
      *                      ),
      *                      @OA\Property(
-     *                         property="GoogleLogin",
+     *                         property="google_login",
      *                         type="Boolean",
      *                         example=false
      *                      ),    
@@ -46,12 +46,12 @@ class UsersettingController extends Controller
      *                         example=true
      *                      ),   
      *                      @OA\Property(
-     *                         property="Two-factor_authentication",
+     *                         property="two-factor_authentication",
      *                         type="Boolean",
      *                         example=false
      *                      ), 
      *                      @OA\Property(
-     *                         property="filtteringTags",
+     *                         property="filtering_tags",
      *                         type="string",
      *                         example="summer,winter,sunny"
      *                      ),                       
@@ -61,7 +61,37 @@ class UsersettingController extends Controller
      * )
      */
 
-    public function AccountSetting(){
+    public function accountSettings()
+    {
+    }
 
+    /**
+     * @OA\Delete(
+     *   path="/settings/account/delete",
+     *   summary="Delete User Account",
+     *   description="This method is used to delete the account of the authenticated user.",
+     *   operationId="deleteAccount",
+     *   tags={"Settings"},
+     *
+     *   @OA\Response(
+     *     response=401,
+     *     description="Unauthenticated",
+     *   ),
+     *  
+     *   @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(property="meta", type="object",
+     *         @OA\Property(property="status", type="integer", example=200),
+     *         @OA\Property(property="msg", type="string", example="OK"),
+     *       )
+     *     )
+     *   )
+     * )
+     */
+    public function deleteAccount()
+    {
     }
 }
