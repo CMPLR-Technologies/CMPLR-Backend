1ee0eac433ff1fd7f67035743d0629b86748e82e
YousefAtefB <YousefAtef.b.a.t@gmail.com>
2021-11-16 06:17:21 +0200

    added create/delete blog routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Create/Delete blog
+Rout::post('/blog',[UserBlogController::class,'create']);
+Rout::delete('/blog','UserBlogController@destroy');
