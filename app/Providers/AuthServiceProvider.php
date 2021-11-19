cffc3893780d2ab48b12e3c3870172e74e6db4e1
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-11-19 14:11:19 +0200

    solving conflict error

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,7 +31,8 @@ class AuthServiceProvider extends ServiceProvider
 
          // call passport:routes() here
             Passport::routes();
-        }
+        
+
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             $spaUrl = "http://spa.test?email_verify_url=".$url;
 
