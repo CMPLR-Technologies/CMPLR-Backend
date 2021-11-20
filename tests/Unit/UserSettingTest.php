a3daa5f22a39d08af3f6d57d24855a375a22c8b1
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-11-20 22:05:28 +0200

    adding user setting test

---
+++ tests/Unit/UserSettingTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Services\User\UserSettingService; 
+use Tests\TestCase;
+
+class UserSettingTest extends TestCase
+{
+    /**
+     * A basic Punit test example.
+     *
+     * @return void
+     */
+    public function test_getting_setting()
+    {
+        $user_id = User::take(1)->first()->value('id');
+        $check = (new UserSettingService() )->GetSettings($user_id);
+        $this->assertnotNull($check);
+    }
+}
