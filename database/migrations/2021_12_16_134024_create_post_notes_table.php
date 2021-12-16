03c2b6db739c85d44f5196966f331e36372dafc4
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-12-16 16:18:47 +0200

    post notes

---
+++ database/migrations/2021_12_16_134024_create_post_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->references('id')->on('posts')->constrained()->onDelete('cascade');
+            $table->text('type'); // like - reply - reblog -comment
+            $table->text('content')->nullable();
+            $table->primary(['user_id' ,'post_id' ]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_notes');
+    }
+}
