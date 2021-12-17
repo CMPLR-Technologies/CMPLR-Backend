f1e9e3d42c27501b46a0cee80281bcf98f979f4d
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-12-17 20:52:53 +0200

    adding post notes service

--- app/Http/Controllers/PostNotesController.php
+++ app/Http/Controllers/PostNotesController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
-use App\Models\BlogSettings;
 use App\Models\PostNotes;
+use App\Services\Post\PostNotesService;
 use Illuminate\Http\Request;
-
-use function GuzzleHttp\Promise\each;
 
 class PostNotesController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | PostNotesController Controller 
+    |--------------------------------------------------------------------------|
+    | This controller handles getting notes for specific post  
+    |
+   */
+    protected $postNotesService;
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(PostNotesService $postNotesService)
+    {
+        $this->postNotesService = $postNotesService;
+    }
+
  /**
      * @OA\GET(
      * path="/post/notes}",
@@ -95,42 +111,15 @@ class PostNotesController extends Controller
      */
     public function getNotes(Request $request )
     {
-        $post_id = (int)$request->post_id;
-
-        $notes = PostNotes::where('post_id' , $post_id)->with('user')->get();
-        $blogs_id =array();
-        foreach($notes as $note)
-        {
-            $blogs_id[] = $note->user->primary_blog_id ;
-        }
-
-        $blogs_data = Blog::whereIn('id', $blogs_id)->with(['settings' => function($query){
-            $query->select('blog_id','avatar' ,'avatar_shape');
-        }])->get();
-
-        // result 
-        $bloghashdata=[];
-        foreach ($blogs_data as $data)
-        {
-            $bloghashdata[$data->id]= $data ;
-        }
+        $postId = (int)$request->post_id;
+        $notes = $this->postNotesService->GetPostNotes($postId);
+        $blogsId =  $this->postNotesService->GetBlogsId($notes);
+        $blogsData = $this->postNotesService->GetBlogsData($blogsId);
+        $blogHashData= $this->postNotesService->HashBlogData($blogsData);
+        $result = $this->postNotesService->GetNotesResult($notes , $blogHashData);
        
-        $result = [] ;
-        for ($i=0; $i<count($notes); $i++) 
-        {
-            $result[] =[
-                'post_id'=>$notes[$i]->post_id,
-                'type' => $notes[$i]->type,
-                'content'=> $notes[$i]->content,
-                'timestamp'=>$notes[$i]->created_at,
-                'blog_name'=>$bloghashdata[$notes[$i]->user_id]->blog_name,
-                'blog_url'=>$bloghashdata[$notes[$i]->user_id]->url,
-                'avatar'=>$bloghashdata[$notes[$i]->user_id]->settings->avatar ,
-                'avatar_shape'=>$bloghashdata[$notes[$i]->user_id]->settings->avatar_shape,
-
-            ];
-        }
-        return response()->json([ $result] , 200);
+    
+        return response()->json([$result] , 200);
 
     }
 
---
+++ app/Services/Post/PostNotesService.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services\Post;
+
+use App\Models\Blog;
+use App\Models\PostNotes;
+
+class PostNotesService
+{
+    /**
+     * geting post notes for specific post.
+     * 
+     * @param integer $postId
+     * 
+     * @return array $notes
+     */
+    public function GetPostNotes($postId)
+    {
+        return PostNotes::where('post_id', $postId)->with('user')->get();
+    }
+
+    /**
+     * Get User data needed
+     *
+     * @param PostNotes $notes
+     * 
+     * @return  $blogs_id
+     */
+    public function GetBlogsId($notes)
+    {
+        $blogsId = array();
+        foreach ($notes as $note) {
+            $blogsId[] = $note->user->primary_blog_id;
+        };
+        return $blogsId;
+    }
+
+    /**
+     * Get blogs data needed 
+     *
+     * @param array $blogsId
+     * 
+     * @return array
+     */
+    public function GetBlogsData($blogsId)
+    {
+        return Blog::whereIn('id', $blogsId)->with(['settings' => function($query){
+            $query->select('blog_id','avatar' ,'avatar_shape');
+        }])->get();;
+    }
+
+    public function HashBlogData ($blogsData)
+    {
+        $blogsHashData=[];
+        foreach ($blogsData as $data)
+        {
+            $blogsHashData[$data->id]= $data ;
+        }
+        return $blogsHashData;
+    }
+    /**
+     * Get Notes result 
+     *
+     * @param array $blogs_id
+     * 
+     * @return arrayofjson
+     */
+    public function GetNotesResult($notes , $blogsHashData)
+    {
+        $result = [] ;
+        for ($i=0; $i<count($notes); $i++) 
+        {
+            $result[] =[
+                'post_id'=>$notes[$i]->post_id,
+                'type' => $notes[$i]->type,
+                'content'=> $notes[$i]->content,
+                'timestamp'=>$notes[$i]->created_at,
+                'blog_name'=>$blogsHashData[$notes[$i]->user_id]->blog_name,
+                'blog_url'=>$blogsHashData[$notes[$i]->user_id]->url,
+                'avatar'=>$blogsHashData[$notes[$i]->user_id]->settings->avatar ,
+                'avatar_shape'=>$blogsHashData[$notes[$i]->user_id]->settings->avatar_shape,
+
+            ];
+        }
+        return $result ;
+    }
+}
