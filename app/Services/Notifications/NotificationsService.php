f9264cec3115ab1e5a72533709249219284c1a2e
YousefAtefB <yousefatef.b.a.t@gmail.com>
2021-12-25 08:08:42 +0200

    push notification

--- app/Services/Notifications/NotificationsService.php
+++ app/Services/Notifications/NotificationsService.php
@@ -5,8 +5,6 @@ namespace App\Services\Notifications;
 use App\Models\Blog;
 use App\Models\Notification;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Mockery\Matcher\Not;
 
 class NotificationsService{
 
@@ -18,20 +16,36 @@ class NotificationsService{
     |
    */
 
+
     /**
      * implements the logic of creating a notification
      * 
      * @return void
      */
-    public function CreateNotification($from_blog_id,$to_blog_id,$type,$post_ask_answer_id)
+    public function CreateNotification($fromBlogId,$toBlogId,$type,$postAskAnswerId)
     {
+        //create the notification in the data base
         Notification::create([
-            'from_blog_id'=>$from_blog_id,
-            'to_blog_id'=>$to_blog_id,
+            'from_blog_id'=>$fromBlogId,
+            'to_blog_id'=>$toBlogId,
             'type'=>$type,
-            'post_ask_answer_id'=>$post_ask_answer_id,
+            'post_ask_answer_id'=>$postAskAnswerId,
             'date'=>Carbon::now()->toRfc850String()
         ]);
+
+        //send a push notification to the corresponding users
+
+        //get tokens of the users who are members of the target blog
+        $usertokens=Blog::find($toBlogId)
+                ->users()
+                ->pluck('fcm_token')
+                ->toArray();
+
+        //build the corresponding message
+        $message="notification: {$fromBlogId} {$type}ed your {$toBlogId}";
+
+        //send the notification
+        NotificationsService::SendNotification($message,$usertokens);
     }
 
     /**
@@ -39,12 +53,12 @@ class NotificationsService{
      * 
      * @return void
      */
-    public function DeleteNotification($from_blog_id,$to_blog_id,$type,$post_ask_answer_id)
+    public function DeleteNotification($fromBlogId,$toBlogId,$type,$postAskAnswerId)
     {
-        Notification::where('from_blog_id',$from_blog_id)
-                    ->where('to_blog_id',$to_blog_id)
+        Notification::where('from_blog_id',$fromBlogId)
+                    ->where('to_blog_id',$toBlogId)
                     ->where('type',$type)
-                    ->where('post_ask_answer_id',$post_ask_answer_id)
+                    ->where('post_ask_answer_id',$postAskAnswerId)
                     ->delete();
     }
 
@@ -144,9 +158,49 @@ class NotificationsService{
      * @return int
      */
 
-    public function SendNotification($user,$token)
+    public function SendNotification($message,$targetTokens)
     {
-        
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $serverKey = 'server key goes here';
+  
+        $data = [
+            "registration_ids" => $targetTokens,
+            "notification" => [
+                "title" => 'notification',
+                "body" => $message,  
+            ]
+        ];
+        $encodedData = json_encode($data);
+    
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+    
+        $ch = curl_init();
+      
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+        // Execute post
+        $result = curl_exec($ch);
+
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }        
+
+        // Close connection
+        curl_close($ch);
+
+        // FCM response
+        // dd($result);        
     }
 
 
