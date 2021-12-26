f0c34f24e89114bcf7f52c15cb6b6e5cd601bd15
Yousif-Ahmed <yousif.lasheen60@gmail.com>
2021-12-27 00:39:15 +0200

    adding socket with message event

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent  implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $sender_id;
+    public $rec_id;
+    public $message;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($sender_id, $rec_id, $msg)
+    {
+        $this->sender_id = $sender_id;
+        $this->rec_id = $rec_id;
+        $this->message = $msg;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return [
+            new PrivateChannel('chat-' . $this->sender_id),
+            new PrivateChannel('chat-'.$this->rec_id)
+
+        ];
+    }
+}
