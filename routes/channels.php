693397e7ab0d332610ca34aa2e2bd56a1d924100
Yousif-Ahmed <yousif.lasheen60@gmail.com>
2021-12-27 22:06:39 +0200

    public channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+/*Broadcast::channel('chat-.{rec_id}', function ($user, $rec_id) {
+    return $user->id === $rec_id;
+});*/
